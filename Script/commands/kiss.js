e279a1d8049602a2383d44eca253fa6734c07d9f
pakhi86643 <pakhi86643@gmail.com>
2026-01-16 14:24:44 +0600

    Update kiss.js

--- Script/commands/kiss.js
+++ Script/commands/kiss.js
@@ -1,97 +1,39 @@
+const request = require("request");
+const fs = require("fs");
+const axios = require("axios");
+
 module.exports.config = {
-    name: "kiss",
-    version: "2.0.0",
-    hasPermssion: 0,
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
-    description: "Kiss the person you want",
-    commandCategory: "Love",
-    usages: "kiss [tag]",
-    cooldowns: 5,
-    dependencies: {
-        "axios": "",
-        "fs-extra": "",
-        "path": "",
-        "jimp": ""
-    }
+  name: "kiss",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "SARDAR RDX",
+  description: "Kiss the tagged person",
+  commandCategory: "Love",
+  usages: "[tag]",
+  cooldowns: 5,
 };
 
-module.exports.onLoad = async () => {
-    const { resolve } = global.nodemodule["path"];
-    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
-    const { downloadFile } = global.utils;
-    const dirMaterial = __dirname + `/cache/`;
-    const path = resolve(__dirname, 'cache', 'hon0.jpeg');
-    if (!existsSync(dirMaterial + "")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/j96ooUs.jpeg", path);
-}
+module.exports.run = async ({ api, event, Threads, global }) => {
+  var link = [    
+    "https://i.postimg.cc/yxDKkJyH/02d4453f3eb0a76a87148433395b3ec3.gif",
+    "https://i.postimg.cc/nLTf2Kdx/1483589602-6b6484adddd5d3e70b9eaaaccdf6867e.gif",
+    "https://i.postimg.cc/Wpyjxnsb/574fcc797b21e-1533876813029926506824.gif",
+    "https://i.postimg.cc/xdsT8SVL/kiss-anime.gif",
+  ];
 
-async function makeImage({ one, two }) {
-    const fs = global.nodemodule["fs-extra"];
-    const path = global.nodemodule["path"];
-    const axios = global.nodemodule["axios"];
-    const jimp = global.nodemodule["jimp"];
-    const __root = path.resolve(__dirname, "cache");
+  var mention = Object.keys(event.mentions);
+  if (!mention || mention.length === 0) 
+    return api.sendMessage("༻﹡﹡﹡﹡﹡﹡﹡༺\n\n𝗣𝗹𝗲𝗮𝘀𝗲 𝘁𝗮𝗴 𝗼𝗻𝗲 𝗽𝗲𝗿𝘀𝗼𝗻 ❗\n\n༻﹡﹡﹡﹡﹡﹡﹡༺", event.threadID, event.messageID);
 
-    let hon_img = await jimp.read(__root + "/hon0.jpeg");
-    let pathImg = __root + `/hon0_${one}_${two}.jpeg`;
-    let avatarOne = __root + `/avt_${one}.png`;
-    let avatarTwo = __root + `/avt_${two}.png`;
+  let tag = event.mentions[mention].replace("@", "");
 
-    let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
-    fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
+  var callback = () => api.sendMessage({
+    body: `⚝──⭒─⭑─⭒──⚝\n\n${tag}, 𝐁𝐚𝐞 𝐠𝐢𝐯𝐞 𝐦𝐞 𝐚 𝐬𝐰𝐞𝐞𝐭 𝐤𝐢𝐬𝐬 💞\n\n⚝──⭒─⭑─⭒──⚝`,
+    mentions: [{ tag: tag, id: Object.keys(event.mentions)[0] }],
+    attachment: fs.createReadStream(__dirname + "/cache/honkiss.gif")
+  }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/honkiss.gif"));  
 
-    let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
-    fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
-
-    let circleOne = await jimp.read(await circle(avatarOne));
-    let circleTwo = await jimp.read(await circle(avatarTwo));
-    hon_img.resize(700, 440).composite(circleOne.resize(150, 150), 390, 23).composite(circleTwo.resize(150, 150), 115, 130);
-
-    let raw = await hon_img.getBufferAsync("image/png");
-
-    fs.writeFileSync(pathImg, raw);
-    fs.unlinkSync(avatarOne);
-    fs.unlinkSync(avatarTwo);
-
-    return pathImg;
-}
-
-async function circle(image) {
-    const jimp = require("jimp");
-    image = await jimp.read(image);
-    image.circle();
-    return await image.getBufferAsync("image/png");
-}
-
-module.exports.run = async function ({ event, api, args, Currencies }) {
-    const fs = global.nodemodule["fs-extra"];
-    const hc = Math.floor(Math.random() * 101);
-    const rd = Math.floor(Math.random() * 100000) + 100000;
-    const { threadID, messageID, senderID } = event;
-    const mention = Object.keys(event.mentions);
-    var one = senderID, two = mention[0];
-    await Currencies.increaseMoney(event.senderID, parseInt(hc * rd));
-
-    const captions = [
-        "কারণে অকারণে প্রতিদিন নিয়ম করে, তোমার মায়াতে জড়িয়ে পড়ছি আমি বারেবার!🌷",
-        "তোমাকে কেন ভালোবাসি তার কোন বিশেষ কারণ আমার জানা নাই! কিন্তু তোমার কাছে সারাজীবন থেকে যাওয়ার হাজারটা কারণ আমার কাছে আছে!💚",
-        "তোমার সাথে কাটানো সময়গুলোর কথা চিন্তা করলে মনে হয়, এই এক জনম তোমার সাথে অনেক কম সময়!😘",
-        "প্রিয় তুমি কি আমার জীবনের সেই গল্প হবে? যেই গল্পের শুরু থাকবে, কিন্তু কোনো শেষ থাকবে না!♥️",
-        "তুমি পাশে থাকলে সবকিছু সুন্দর মনে হয়, জীবন যেন একটা মধুর কবিতায় রূপ নেয়!😍",
-        "তোমাকে ছাড়া জীবনটা অসম্পূর্ণ, তুমি আমার ভালোবাসার পূর্ণতা!🧡",
-        "তুমি আমার স্বপ্ন, তুমি আমার জীবনের প্রতিটি সুন্দর মুহূর্ত!🌻",
-        "আমার চোখে তোমার অস্থিত্ব খোঁজতে এসোনা, হারিয়ে যাবে! কেননা আমার পুরোটা-জুরেই তোমারই নির্বাক আনাগোনা!🌺",
-        "তোমাতে শুরু তোমাতেই শেষ, তুমি না থাকলে আমাদের গল্প এখানেই শেষ!😘",
-        "ভালোবাসা যদি কোনো অনুভূতি হয়, তাহলে তোমার প্রতি আমার অনুভূতি পৃথিবীর সেরা অনুভূতি।🌻ღ🌺"
-    ];
-
-    const caption = captions[Math.floor(Math.random() * captions.length)];
-
-    if (!two) return api.sendMessage("Please tag 1 person", threadID, messageID);
-    else {
-        return makeImage({ one, two }).then(path => api.sendMessage({
-            body: `${caption}`,
-            attachment: fs.createReadStream(path)
-        }, threadID, () => fs.unlinkSync(path), messageID));
-    }
-}
+  return request(encodeURI(link[Math.floor(Math.random() * link.length)]))
+    .pipe(fs.createWriteStream(__dirname + "/cache/honkiss.gif"))
+    .on("close", () => callback());
+};
